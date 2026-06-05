6148958f29f6e4875850c9a456ba0ca0c9567f9f
Praveen <hello@praveentechworld.com>
2026-06-05 23:24:08 +0400

    SEO: FAQ JSON-LD, article og:type, search noindex, twitter tags, RSS improvements

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,5 +1,4 @@
 import { getCollection } from "astro:content";
-import { calculateReadingTime } from "@lib/utils/readingTime";
 
 export async function GET() {
   const articles = await getCollection("articles");
@@ -11,6 +10,10 @@ export async function GET() {
         new Date(a.data.publishDate).getTime()
     );
 
+  const lastBuild = published.length > 0
+    ? new Date(published[0].data.publishDate).toUTCString()
+    : new Date().toUTCString();
+
   const items = published.map(
     (article) => `
     <item>
@@ -20,18 +23,24 @@ export async function GET() {
       <guid>https://www.praveentechworld.com/blog/${article.id}</guid>
       <pubDate>${new Date(article.data.publishDate).toUTCString()}</pubDate>
       <category>${article.data.category}</category>
+      <dc:creator><![CDATA[Praveen]]></dc:creator>
+      ${article.data.coverImage ? `<media:content url="${article.data.coverImage}" medium="image" />` : ""}
     </item>`
   );
 
   return new Response(
     `<?xml version="1.0" encoding="UTF-8"?>
-    <rss version="2.0" xmlns:atom="http://www.w3.org/2005/Atom">
+    <rss version="2.0"
+      xmlns:atom="http://www.w3.org/2005/Atom"
+      xmlns:dc="http://purl.org/dc/elements/1.1/"
+      xmlns:media="http://search.yahoo.com/mrss/">
       <channel>
         <title>PraveenTechWorld</title>
         <description>Practical Technology Knowledge Base</description>
         <link>https://www.praveentechworld.com</link>
         <atom:link href="https://www.praveentechworld.com/rss.xml" rel="self" type="application/rss+xml"/>
         <language>en</language>
+        <lastBuildDate>${lastBuild}</lastBuildDate>
         ${items.join("")}
       </channel>
     </rss>`,
